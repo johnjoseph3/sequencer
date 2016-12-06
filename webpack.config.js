dee3697cce70db54b38cc989d5cc2675c3a0c56d
John <jjoseph@practichem.com>
2016-12-06 11:02:43 -0500

    Setup Webpack, Babel, and JSHINT.

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+	entry: "./app.js",
+	output: {
+		filename: "bundle.js"
+	},
+	module: {
+		preLoaders: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'jshint-loader'
+			}
+		],
+		loaders: [
+			{
+				test: /\.es6$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+				query: {
+					presets: ['es2015']
+				}
+			}
+		]
+	},
+	resolve: {
+		extensions: ['', '.js', '.es6']
+	},
+}
