809fa181a1d3ec872aeb47a429f76f04323919d1
John <jjoseph@practichem.com>
2016-12-09 10:48:36 -0500

    Add drumPad controller.

--- client/app.js
+++ client/app.js
@@ -1,18 +1,16 @@
-require('file?name=[name].[ext]!./index.html');
-require("./stylesheet.css");
-
 import angular from 'angular';
 import uirouter from 'angular-ui-router';
+const app = angular.module('app', [uirouter]);
 
-let app = angular.module('app', [uirouter]);
+require('file?name=[name].[ext]!./index.html');
+require('./stylesheet.css');
+require('./drum-pad.js');
 
 app.config(function($stateProvider, $urlRouterProvider) {
 	$urlRouterProvider.otherwise('/home');
-
 	$stateProvider
 		.state('home', {
 			url: '/home',
-			template: require("html!./test.html")
+			template: require("html!./drum-pad.html"),
 		});
-
 });
---
+++ client/drum-pad.js
@@ -0,0 +1,3 @@
+angular.module('app').controller('drumPad', ['$scope', function($scope){
+		$scope.name = "John";
+}]);
