9f09875a79065d1d5f57768680e4d39c35307887
John <jjoseph@practichem.com>
2016-12-09 14:58:37 -0500

    Add drum sounds.

--- client/drum-pad.js
+++ client/drum-pad.js
@@ -1,6 +1,10 @@
+import {sounds} from "./load-tracks.js";
+
 angular.module('app').controller('drumPad', ['$scope', function($scope){
-		$scope.name = "John";
-		$scope.getDrum = function() {
-			return require('../public/Drum1.mp3');
+		$scope.soundNumbers = ["01", "02", "03", "04", "05", "06", "07", "08"];
+
+		$scope.getSound = function(soundNumber) {
+			return require(`../public/AB_Clap-${soundNumber}.wav`);
 		};
+
 }]);
