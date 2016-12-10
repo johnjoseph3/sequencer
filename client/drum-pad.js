180b758898bdf0fe10386c48cde4a92d2c3f7538
John Joseph <jmj1444@gmail.com>
2016-12-10 18:40:18 -0500

    Add buttons for sound selectors.

--- client/drum-pad.js
+++ client/drum-pad.js
@@ -1,40 +1,28 @@
-let bpm = 150;
-let bpmInMilliseconds = 60000/bpm;
-
-let timeElapsedInMilliseconds = 0;
-let timeTotalInMilliseconds = 60000;
-
-let updateTimeMeter = function() {
-	if(timeElapsedInMilliseconds < timeTotalInMilliseconds) {
-		timeElapsedInMilliseconds += 10;
-		if (timeElapsedInMilliseconds%bpmInMilliseconds === 0) {
-			console.log("BEAT");
-		}
-		let timeIndicatorWidth = `${(timeElapsedInMilliseconds/timeTotalInMilliseconds)*100}%`;
-		let timeIndicator = angular.element(document.querySelector('.time-indicator'));
-		timeIndicator.css('width', timeIndicatorWidth);
-
-		// Temp
-		let seconds = angular.element(document.querySelector('.seconds'));
-		timeIndicator.text(timeElapsedInMilliseconds);
-	}
-};
-
-// CONTROLLER
 angular.module('app').controller('drumPad', ['$scope', ($scope) => {
+	$scope.bpm = 120;
+	// FIXME Find a more dynamic way to retrieve files.
+	$scope.soundFiles = [];
+	let soundFileIndices = ["01", "02", "03", "04", "05", "06", "07", "08"];
+	for(let soundFileIndex of soundFileIndices) {
+		$scope.soundFiles.push(require(`../public/AB_Clap-${soundFileIndex}.wav`));
+	}
 
-	// FIXME Find a better more dynamic way to retrieve files.
-	$scope.soundFileIndices = ["01", "02", "03", "04", "05", "06", "07", "08"];
+	// $scope.beat = [];
 
-	$scope.getSound = (soundFileIndex) => {
-		return require(`../public/AB_Clap-${soundFileIndex}.wav`);
+	$scope.startBeat = () => {
+		let bpmInMilliseconds = 60000/parseInt($scope.bpm);
+		let currentBeatIndex = 0;
+
+		setInterval(function() {
+			if(currentBeatIndex < soundFiles.length){
+				new Audio(soundFiles[currentBeatIndex]).play();
+				currentBeatIndex++;
+			}	else {
+				currentBeatIndex = 0;
+				new Audio(soundFiles[currentBeatIndex]).play();
+				currentBeatIndex++;
+			}
+		}, bpmInMilliseconds);
 	};
 
-	$scope.startBeat = () =>
-		setInterval(updateTimeMeter, 10);
-
 }]);
-
-// check to see if user defined bpm
-// let bpm = 80;
-// milliseconds = bpm/60000;
