4318ac98fad67c8cfac34e9c623869a5d338a9b0
John Joseph <jmj1444@gmail.com>
2016-12-10 15:10:34 -0500

    Add bpm measurement.

--- client/drum-pad.js
+++ client/drum-pad.js
@@ -1,3 +1,26 @@
+let bpm = 150;
+let bpmInMilliseconds = 60000/bpm;
+
+let timeElapsedInMilliseconds = 0;
+let timeTotalInMilliseconds = 60000;
+
+let updateTimeMeter = function() {
+	if(timeElapsedInMilliseconds < timeTotalInMilliseconds) {
+		timeElapsedInMilliseconds += 10;
+		if (timeElapsedInMilliseconds%bpmInMilliseconds === 0) {
+			console.log("BEAT");
+		}
+		let timeIndicatorWidth = `${(timeElapsedInMilliseconds/timeTotalInMilliseconds)*100}%`;
+		let timeIndicator = angular.element(document.querySelector('.time-indicator'));
+		timeIndicator.css('width', timeIndicatorWidth);
+
+		// Temp
+		let seconds = angular.element(document.querySelector('.seconds'));
+		timeIndicator.text(timeElapsedInMilliseconds);
+	}
+};
+
+// CONTROLLER
 angular.module('app').controller('drumPad', ['$scope', ($scope) => {
 
 	// FIXME Find a better more dynamic way to retrieve files.
@@ -7,4 +30,11 @@ angular.module('app').controller('drumPad', ['$scope', ($scope) => {
 		return require(`../public/AB_Clap-${soundFileIndex}.wav`);
 	};
 
+	$scope.startBeat = () =>
+		setInterval(updateTimeMeter, 10);
+
 }]);
+
+// check to see if user defined bpm
+// let bpm = 80;
+// milliseconds = bpm/60000;
